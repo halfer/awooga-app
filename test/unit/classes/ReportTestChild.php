e5b7e4242eeff4fa476291aafa26306d692f5b18
Jon Hinks <projects.ws@jondh.me.uk>
2014-11-18 16:38:00 +0000

    Make a start on Report tests

---
+++ test/unit/classes/ReportTestChild.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Awooga\Testing;
+
+class ReportTestChild extends \Awooga\Report
+{
+	public function getProperty($property)
+	{
+		return $this->$property;
+	}
+
+	/**
+	 * Special rules for URLs - if there's just one we return it as a string
+	 * 
+	 * @return mixed
+	 */
+	public function getUrl()
+	{
+		if (count($this->urls) == 1)
+		{
+			return $this->urls[0];
+		}
+		else
+		{
+			return $this->urls;
+		}
+	}
+}
--- test/unit/tests/ReportTest.php
+++ test/unit/tests/ReportTest.php
@@ -4,49 +4,126 @@ namespace Awooga\Testing;
 
 class ReportTest extends \PHPUnit_Framework_TestCase
 {
+	public function setUp()
+	{
+		$root = realpath(__DIR__ . '/../../..');
+
+		require_once $root . '/src/classes/Report.php';
+		require_once $root . '/src/classes/TrivialException.php';
+		require_once $root . '/test/unit/classes/ReportTestChild.php';
+	}
+
 	public function testSetTitle()
 	{
-		
+		$report = new ReportTestChild(1);
+		$title = 'Set title';
+		$report->setTitle($title);
+		$this->assertEquals($title, $report->getProperty('title'));
 	}
 
+	/**
+	 * Make sure null/empty titles are rejected
+	 * 
+	 * @expectedException \Awooga\Exceptions\TrivialException
+	 */
 	public function testNoTitle()
 	{
-		
+		$report = new ReportTestChild(1);
+		$report->setTitle(null);
 	}
 
+	/**
+	 * Make sure non-strings are rejected
+	 * 
+	 * @expectedException \Awooga\Exceptions\TrivialException
+	 */
 	public function testTitleOfBadType()
 	{
-		
+		$report = new ReportTestChild(1);
+		$report->setTitle(null);
+		$report->setTitle(new \stdClass());
 	}
 
+	/**
+	 * Make sure a URL string can be set
+	 */
 	public function testSetUrlString()
 	{
-		
+		$report = new ReportTestChild(1);
+		$url = 'http://example.com/thing';
+		$report->setUrl($url);
+
+		$this->assertEquals($url, $report->getUrl());
 	}
 
 	public function testSetGoodUrlArray()
 	{
-		
+		$report = new ReportTestChild(1);
+		$urls = array(
+			'http://example.com/one',
+			'http://example.com/two',
+		);
+		$report->setUrl($urls);
+		$this->assertEquals($urls, $report->getUrl());
 	}
 
-	public function testSetBadUrlArray()
+	/**
+	 * Make sure empty strings in URL arrays are rejected
+	 * 
+	 * @expectedException \Awooga\Exceptions\TrivialException
+	 */
+	public function testSetArrayContainingEmptyUrls()
 	{
+		$report = new ReportTestChild(1);
+		$urls = array(
+			'',
+			'http://example.com/two',
+		);
+		$report->setUrl($urls);
+	}
+
+	/**
+	 * Make sure non-strings in URL arrays are rejected
+	 * 
+	 * @expectedException \Awooga\Exceptions\TrivialException
+	 */
+	public function testSetArrayContainingUrlsOfWrongType()
+	{
+		$report = new ReportTestChild(1);
+		$urls = array(
+			'http://example.com/something',
+			5,
+		);
+		$report->setUrl($urls);
 		
 	}
 
+	/**
+	 * Checks the report can accept and store a description string
+	 */
 	public function testSetGoodDescription()
 	{
-		
+		$report = new ReportTestChild(1);
+		$description = 'This is a description';
+		$report->setDescription($description);
+		$this->assertEquals($description, $report->getProperty('description'));
 	}
 
+	/**
+	 * Make sure empty descriptions are rejected
+	 * 
+	 * @expectedException \Awooga\Exceptions\TrivialException
+	 */
 	public function testSetEmptyDescription()
 	{
-		
+		$report = new ReportTestChild(1);
+		$report->setDescription(null);
 	}
 
 	public function testSetDescriptionOfBadType()
 	{
-		
+		$report = new ReportTestChild(1);
+		$report->setDescription(6);		
 	}
 
 	public function testSetGoodIssues()
