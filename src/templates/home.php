bcce6fc1b88a1576ed7a2f237e2816a0fd123bed
Jon Hinks <projects.ws@jondh.me.uk>
2014-11-28 19:10:29 +0000

    Update FAQ on home page

--- src/templates/home.php
+++ src/templates/home.php
@@ -6,8 +6,9 @@
 <div class="jumbotron">
 	<h1>Awooga</h1>
 	<p>
-		Awooga is a website to warn beginner coders of problems with non-optimal teaching materials
-		&mdash; just type in the address and see if it's in the database.
+		Awooga is a website designed to warn coders of problems with non-optimal teaching
+		materials &mdash; especially in relation to security issues. Just type in the
+		address and see if it's in the database.
 	</p>
 
 	<form>
@@ -38,38 +39,71 @@
 	</form>
 </div>
 
+<h2>Frequently asked questions</h2>
+
 <div class="row">
 	<div class="col-md-4">
+		<h3>
+			What does a listing mean?
+		</h3>
 		<p>
-			Why do we do it?
+			A listing in this database means that, in the opinion of the editor who added it,
+			the referenced tutorial has a number of security or quality issues, and thus that
+			beginners need to be cautious about using that material.
 		</p>
 	</div>
 	<div class="col-md-4">
+		<h3>
+			How can I contribute?
+		</h3>
+
 		<p>
-			Let's have some info here about how to contribute.
+			If you are an experienced programmer, I'd love to have your help. Please create a
+			public Git repository <a href="#">using this template</a> and let me take a look. If I
+			agree with your initial commits, I'll add your repo to auto-publish.
+			You'll need to be willing to keep your list maintained - if a tutorial is improved,
+			the database needs to reflect that.
 		</p>
 	</div>
 	<div class="col-md-4">
+		<h3>
+			How should reports be written?
+		</h3>
 		<p>
-			Also the last 10 reports in the database
+			Reports should treat problematic teaching material as a bug to be fixed. We ask
+			all editors to remain polite with their writing, and to employ a positive, can-do
+			tone.
 		</p>
 	</div>
 </div>
 
 <div class="row">
 	<div class="col-md-4">
+		<h3>
+			How should authors respond?
+		</h3>
 		<p>
-			Instructions for tutorial authors
+			If you are listed, don't panic! If you agree with the assessment made of your resource,
+			you can either fix the mistakes in it, take it down temporarily whilst you fix it,
+			or remove it entirely. Remember that sometimes taking down really old material is
+			sometimes the right thing to do. Feel free to liaise with the editor, too.
 		</p>
 	</div>
 	<div class="col-md-4">
+		<h3>
+			What happens if a tutorial is improved?
+		</h3>
 		<p>
-			What issues are we most interested in?
+			If you've made a change as a result of a report here, thank you! Please ask the editor
+			to review the changes, and to mark the issue(s) as resolved.
 		</p>
 	</div>
 	<div class="col-md-4">
+		<h3>
+			Can we comment on resources via the Awooga website?
+		</h3>
 		<p>
-			What features are we working on?
+			Not yet, but it's a good idea! If there's demand I'll add that to the to-list.
 		</p>
 	</div>
 </div>
