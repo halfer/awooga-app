05e08fa2fd5b87e39517bbd64bd95f647625d26d
Jon Hinks <projects.ws@jondh.me.uk>
2014-11-28 17:46:15 +0000

    Add in secondary links to reports screen

--- src/templates/browse.php
+++ src/templates/browse.php
@@ -15,6 +15,20 @@
 						nofollow="nofollow"
 						target="_blank"
 					><?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?></a>
+
+					<?php // If there are more URLs, list them here ?>
+					<?php if (count($report['urls']) > 1): ?>
+						<small>[
+							<?php for($urlId = 1; $urlId < count($report['urls']); $urlId++): ?>
+								<a
+									href="<?php echo htmlentities($report['urls'][$urlId], ENT_HTML5, 'UTF-8') ?>"
+									alt="Secondary link for this resource"
+									nofollow="nofollow"
+									target="_blank"
+								>Secondary link</a>
+							<?php endfor ?>
+						]</small>
+					<?php endif ?>
 				<?php else: ?>
 					<?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?>
 				<?php endif ?>
