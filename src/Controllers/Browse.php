6d7c99f53eb75a96263c7f3791ca9e2bb42f62f8
Jon Hinks <projects.ws@jondh.me.uk>
2014-11-28 18:08:22 +0000

    Refactor a bit, add in issues into reports list

--- src/Controllers/Browse.php
+++ src/Controllers/Browse.php
@@ -10,8 +10,6 @@ class Browse extends BaseController
 
 	/**
 	 * Controller for report browsing
-	 * 
-	 * @todo For each report, add in a urls array
 	 */
 	public function execute()
 	{
@@ -23,7 +21,7 @@ class Browse extends BaseController
 			$this->pageRedirectAndExit($pageNumber ? 'browse/' . $pageNumber : 'browse');
 		}
 
-		$reports = $this->getReports($pageSize);
+		$reports = $this->getReportsWithRelatedTables($pageSize);
 		echo $this->render(
 			'browse',
 			array(
@@ -34,52 +32,87 @@ class Browse extends BaseController
 		);
 	}
 
+	protected function getReportsWithRelatedTables($pageSize)
+	{
+		$reports = $this->getReports($pageSize);
+
+		// Convert the reports to an pk-indexed array
+		$reportIds = array();
+		$reportsById = array();
+		foreach ($reports as $report)
+		{
+			$id = $report['id'];
+			$reportsById[$id] = $report;
+
+			// Add in an empty field for URLs and issues
+			$reportsById[$id]['urls'] = array();
+			$reportsById[$id]['issues'] = array();
+
+			// Let's also get a list of report PKs
+			$reportIds[] = $id;
+		}
+
+		// Add each link to the right report
+		foreach($this->getRelatedUrls($reportIds) as $url)
+		{
+			$reportsById[$url['report_id']]['urls'][] = $url['url'];
+		}
+
+		// Add each issue to the right report
+		foreach($this->getRelatedIssues($reportIds) as $issue)
+		{
+			$reportsById[$issue['report_id']]['issues'][] = $issue['issue_code'];
+		}
+
+		return $reportsById;
+	}
+
 	protected function getReports($pageSize)
 	{
 		$limitClause = $this->getLimitClause($pageSize);
-		$sqlReports = "
+		$sql = "
 			SELECT *
 			FROM report
 			WHERE is_enabled = 1
 			ORDER BY id DESC
 			{$limitClause}
 		";
-		$statementReports = $this->getDriver()->prepare($sqlReports);
-		$ok1 = $statementReports->execute();
-		$reports = $statementReports->fetchAll(\PDO::FETCH_ASSOC);
+		$statement = $this->getDriver()->prepare($sql);
+		$ok = $statement->execute();
 
-		$reportIds = array();
-		// Convert the reports to an pk-indexed array
-		$reportsById = array();
-		foreach ($reports as $report)
-		{
-			$id = $report['id'];
-			$reportsById[$id] = $report;
-			$reportsById[$id]['urls'] = array();
+		return $statement->fetchAll(\PDO::FETCH_ASSOC);
+	}
 
-			// Let's also get a list of report PKs
-			$reportIds[] = $id;
-		}
-
-		// Get the related links
+	protected function getRelatedUrls(array $reportIds)
+	{
 		$strIds = implode(',', $reportIds);
-		$sqlUrls = "
-			SELECT *
+		$sql = "
+			SELECT report_id, url
 			FROM resource_url
 			WHERE report_id IN ({$strIds})
 			/* Get them in order of creation, first one is regarded as 'primary' */
 			ORDER BY id
 		";
-		$statementLinks = $this->getDriver()->prepare($sqlUrls);
-		$ok2 = $statementLinks->execute();
-		$urls = $statementLinks->fetchAll(\PDO::FETCH_ASSOC);
+		$statement = $this->getDriver()->prepare($sql);
+		$ok = $statement->execute();
 
-		// Add each link to the right report
-		foreach($urls as $url)
-		{
-			$reportsById[$url['report_id']]['urls'][] = $url['url'];
-		}
+		return $statement->fetchAll(\PDO::FETCH_ASSOC);
+	}
 
-		return $reportsById;
+	protected function getRelatedIssues(array $reportIds)
+	{
+		$strIds = implode(',', $reportIds);
+		$sql = "
+			SELECT r.report_id, i.code issue_code
+			FROM report_issue r
+			INNER JOIN issue i ON (i.id = r.issue_id)
+			WHERE r.report_id IN ({$strIds})
+			/* Get them in order of type */
+			ORDER BY r.issue_id
+		";
+		$statement = $this->getDriver()->prepare($sql);
+		$ok = $statement->execute();
+
+		return $statement->fetchAll(\PDO::FETCH_ASSOC);
 	}
 }
--- src/templates/browse.php
+++ src/templates/browse.php
@@ -4,40 +4,54 @@
 ) ?>
 
 <table class="table">
-	<?php foreach ($reports as $report): ?>
+	<thead>
 		<tr>
-			<td>
-				<?php // If there is a 0th URL (there should be) let's use that ?>
-				<?php if (isset($report['urls'][0])): ?>
-					<a
-						href="<?php echo htmlentities($report['urls'][0], ENT_HTML5, 'UTF-8') ?>"
-						alt="Primary link for this resource"
-						nofollow="nofollow"
-						target="_blank"
-					><?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?></a>
-
-					<?php // If there are more URLs, list them here ?>
-					<?php if (count($report['urls']) > 1): ?>
-						<small>[
-							<?php for($urlId = 1; $urlId < count($report['urls']); $urlId++): ?>
-								<a
-									href="<?php echo htmlentities($report['urls'][$urlId], ENT_HTML5, 'UTF-8') ?>"
-									alt="Secondary link for this resource"
-									nofollow="nofollow"
-									target="_blank"
-								>Secondary link</a>
-							<?php endfor ?>
-						]</small>
-					<?php endif ?>
-				<?php else: ?>
-					<?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?>
-				<?php endif ?>
-			</td>
-			<td>
-				<?php echo htmlentities($report['description'], ENT_HTML5, 'UTF-8') ?>
-			</td>
+			<th>Links</th>
+			<th>Issues</th>
+			<th>Description</th>
 		</tr>
-	<?php endforeach ?>
+	</thead>
+	<tbody>
+		<?php foreach ($reports as $report): ?>
+			<tr>
+				<td>
+					<?php // If there is a 0th URL (there should be) let's use that ?>
+					<?php if (isset($report['urls'][0])): ?>
+						<a
+							href="<?php echo htmlentities($report['urls'][0], ENT_HTML5, 'UTF-8') ?>"
+							alt="Primary link for this resource"
+							nofollow="nofollow"
+							target="_blank"
+						><?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?></a>
+
+						<?php // If there are more URLs, list them here ?>
+						<?php if (count($report['urls']) > 1): ?>
+							<small>[
+								<?php for($urlId = 1; $urlId < count($report['urls']); $urlId++): ?>
+									<a
+										href="<?php echo htmlentities($report['urls'][$urlId], ENT_HTML5, 'UTF-8') ?>"
+										alt="Secondary link for this resource"
+										nofollow="nofollow"
+										target="_blank"
+									>Secondary link</a>
+								<?php endfor ?>
+							]</small>
+						<?php endif ?>
+					<?php else: ?>
+						<?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?>
+					<?php endif ?>
+				</td>
+				<td>
+					<?php foreach ($report['issues'] as $issues): ?>
+						<?php echo $issues ?>
+					<?php endforeach ?>
+				</td>
+				<td>
+					<?php echo htmlentities($report['description'], ENT_HTML5, 'UTF-8') ?>
+				</td>
+			</tr>
+		<?php endforeach ?>
+	</tbody>
 </table>
 
 <?php if ($maxPage > 1): ?>
