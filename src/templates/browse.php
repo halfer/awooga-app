c5b2229854c07a6d66598635de72580ddc669595
Jon Hinks <projects.ws@jondh.me.uk>
2014-12-03 11:01:11 +0000

    Split long template with a partial

--- src/templates/browse.php
+++ src/templates/browse.php
@@ -17,62 +17,7 @@
 	</thead>
 	<tbody>
 		<?php foreach ($reports as $report): ?>
-			<tr>
-				<td>
-					<a
-						href="/report/<?php echo $report['id'] ?>"
-						alt="More info"
-					><?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?></a>
-				</td>
-				<td>
-					<?php // If there is a 0th URL (there should be) let's use that ?>
-					<?php if (isset($report['urls'][0])): ?>
-						<a
-							href="<?php echo htmlentities($report['urls'][0], ENT_HTML5, 'UTF-8') ?>"
-							alt="Primary link for this resource"
-							rel="nofollow"
-							target="_blank"
-						>Primary link</a>
-
-						<?php // If there are more URLs, list them here ?>
-						<?php if (count($report['urls']) > 1): ?>
-							<small>[
-								<?php for($urlId = 1; $urlId < count($report['urls']); $urlId++): ?>
-									<a
-										href="<?php echo htmlentities($report['urls'][$urlId], ENT_HTML5, 'UTF-8') ?>"
-										alt="Secondary link for this resource"
-										nofollow="nofollow"
-										target="_blank"
-									>Secondary link</a>
-								<?php endfor ?>
-							]</small>
-						<?php endif ?>
-					<?php else: ?>
-						<?php echo htmlentities($report['title'], ENT_HTML5, 'UTF-8') ?>
-					<?php endif ?>
-				</td>
-				<?php // Here are the issues lozenges ?>
-				<td class="issues">
-					<?php foreach ($report['issues'] as $issue): ?>
-						<?php if ($issue['resolved_at']): ?>
-							<span class="label label-success">
-								<?php echo $issue['code'] ?> (fixed)
-							</span>
-						<?php else: ?>
-							<span class="label label-danger">
-								<?php echo $issue['code'] ?>
-							</span>
-						<?php endif ?>
-					<?php endforeach ?>
-				</td>
-				<td>
-					<?php // Converted from markdown, so should be safe ?>
-					<?php echo $report['description_html'] ?>
-				</td>
-				<td>
-					#<?php echo $report['repository_id'] ?>
-				</td>
-			</tr>
+			<?php $this->insert('partials/browse-row', ['report' => $report, ]) ?>
 		<?php endforeach ?>
 	</tbody>
 </table>
