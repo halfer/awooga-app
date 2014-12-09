c2d3e6d20d34a1120c2bc13f0f6b4f0bc94ffaa1
Jon Hinks <projects.ws@jondh.me.uk>
2014-12-09 15:27:03 +0000

    Add search examples

--- src/templates/partials/search.php
+++ src/templates/partials/search.php
@@ -15,3 +15,14 @@
 		/>
 	</span>
 </div>
+
+<div style="margin-top: 12px;">
+	<small>
+		Examples:
+			<a href="/browse?search=AJAX">keyword</a>,
+			<a href="/browse?search=%22Creating+a+Login+System+in+PHP%22">tutorial title</a> (use quotes),
+			<a href="/browse?search=phppot.com">domain</a>,
+			<a href="/browse?search=http%3A%2F%2Fwww.amitpatil.me%2Fyoutube-like-rating-script-jquery-php%2F">full address</a>,
+			<a href="/browse?search=sql-injection">specific issue</a>.
+	</small>
+</div>
