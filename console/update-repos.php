142cabefc476c1d645a1863bded94e7aa52659dc
Jon Hinks <projects.ws@jondh.me.uk>
2014-12-11 18:04:35 +0000

    Add script time limit

--- console/update-repos.php
+++ console/update-repos.php
@@ -17,10 +17,13 @@ use Awooga\Core\UpdateAll;
 $repoRoot = $root . '/filesystem/mount';
 $searchIndex = $root . '/filesystem/mount/search-index';
 
-// Connect to the database
-// @todo Pull this from env config
+// Connect to the database (@todo Move this to env config)
 $dsn = 'mysql:dbname=awooga;host=localhost;username=awooga_user;password=password';
 $pdo = new PDO($dsn, 'awooga_user', 'password');
+
+// Add a time limit to prevent crons overlapping (@todo Move this to config). Maybe would be
+// better though to detect instance of this script and exit if it is running?
+set_time_limit(60 * 6);
 
 // Set up importer, search indexer, updater
 $importer = new GitImporter($repoRoot, true);
