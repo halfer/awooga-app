23ce3a07c7ece1cdb60e3ac65024eeb76fc04b7d
Jon Hinks <projects.ws@jondh.me.uk>
2014-12-16 10:48:02 +0000

    Add tests for issues

--- test/browser/tests/IssueTest.php
+++ test/browser/tests/IssueTest.php
@@ -7,8 +7,19 @@ require_once realpath(__DIR__ . '/..') . '/classes/TestCase.php';
 
 class IssueTest extends TestCase
 {
+	/**
+	 * Check the issues table works
+	 * 
+	 * @driver phantomjs
+	 */
 	public function testIssues()
 	{
-		// Check counts for each issue
+		// Check number of issues
+		$this->visit(self::DOMAIN . '/issues');
+		$rows = $this->all('table tbody tr');
+		$this->assertEquals(10, count($rows));
+
+		// Check the counts for the first issue
+		$this->assertHasCss('table tbody tr:first-child td:last-child:contains("23")');
 	}
 }
