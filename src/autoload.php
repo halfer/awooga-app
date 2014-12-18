2d9c4b306b143084a40879550c6c6ac5975213e5
Jon Hinks <projects.ws@jondh.me.uk>
2014-12-18 18:34:14 +0000

    Add test paths to custom autoloader

--- src/autoload.php
+++ src/autoload.php
@@ -2,17 +2,95 @@
 
 // Autoload for app-level classes, traits and interfaces
 
+namespace Awooga;
+
+class Autoloader
+{
+	const PREFIX = 'Awooga';
+
+	public function mainLoader($class)
+	{
+		$loaded = false;
+		$slashes = str_replace('\\', '/', $class);
+		$path =
+			$this->getProjectRoot() . '/src/' .
+			str_replace(self::PREFIX . '/', '', $slashes) . '.php';
+		if (file_exists($path))
+		{
+			require_once $path;
+			$loaded = true;
+		}
+
+		return $loaded;
+	}
+
+	/**
+	 * A loader for test classes
+	 */
+	public function testLoader($class)
+	{
+		// Here's the mapping of prefixes to possible paths
+		$namespaces = array(
+			'Awooga\Testing\Browser' => array('test/browser/classes', 'test/browser/tests', ),
+			'Awooga\Testing' => array('test/traits', ),
+			'Awooga\Testing\Unit' => array('test/unit/classes', 'test/unit/tests', ),
+		);
+
+		$loaded = false;
+		foreach ($namespaces as $namespace => $searchPaths)
+		{
+			// If we find a namespace match...
+			if (strpos($class, $namespace) !== false)
+			{
+				// ... let's try finding the class
+				foreach ($searchPaths as $searchPath)
+				{
+					$path =
+						$this->getProjectRoot() . '/' .
+						$searchPath . '/' . $this->getLeafname($class) . '.php';
+					// If it is found, load it and exit out
+					if (file_exists($path))
+					{
+						require_once $path;
+						$loaded = true;
+						break 2;
+					}
+				}
+			}
+		}
+
+		return $loaded;
+	}
+
+	protected function getLeafname($class)
+	{
+		return substr($class, strrpos($class, '\\') + 1);
+	}
+
+	/**
+	 * Is this class in our app namespace?
+	 * 
+	 * @param string $class
+	 * @return boolean
+	 */
+	public function ourNamespace($class)
+	{
+		return substr($class, 0, strlen(self::PREFIX)) == self::PREFIX;
+	}
+
+	protected function getProjectRoot()
+	{
+		return realpath(__DIR__ . '/..');
+	}
+}
+
 spl_autoload_register(
 	function($class)
 	{
-		$root = __DIR__;
-
-		$prefix = 'Awooga';
-		if (substr($class, 0, strlen($prefix)) == $prefix)
+		$loader = new \Awooga\Autoloader();
+		if ($loader->ourNamespace($class))
 		{
-			$slashes = str_replace('\\', '/', $class);
-			$path = $root . '/' . str_replace($prefix . '/', '', $slashes) . '.php';
-			require_once $path;
+			$loader->mainLoader($class) || $loader->testLoader($class);
 		}
 	}
 );
