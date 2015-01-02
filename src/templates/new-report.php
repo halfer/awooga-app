894e41efd1d02a90cf6b8f6bb348ad372b9448bb
Jon Hinks <projects.ws@jondh.me.uk>
2015-01-02 15:06:05 +0000

    Sketch out shape of New Report page

--- src/templates/new-report.php
+++ src/templates/new-report.php
@@ -7,8 +7,93 @@
 	A page to create new reports.
 </p>
 
-<form method="post">
-	<p>This form will save a demo report against this user.</p>
+<form method="post" class="form-horizontal">
+
+	<div class="form-group">
+		<label for="inputUrl" class="col-sm-2 control-label">URL(s):</label>
+		<div class="col-sm-10">
+			<div class="input-group">
+				<input
+					type="text"
+					id="inputUrl"
+					class="form-control"
+					placeholder="The web address(es) for this tutorial, including the http/https protocol"
+				/>
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="button">+</button>
+					<button class="btn btn-default" type="button">-</button>
+				</span>
+			</div>
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<label for="inputTitle" class="col-sm-2 control-label">Title:</label>
+		<div class="col-sm-10">
+			<input
+				type="text"
+				id="inputTitle"
+				class="form-control"
+				placeholder="The title of the resource (you can just copy and paste this from the resource)"
+			/>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label for="reportDescription" class="col-sm-2 control-label">Description:</label>
+		<div class="col-sm-10">
+			<textarea
+				name="description"
+				id="reportDescription"
+				class="form-control"
+				rows="3"
+				placeholder="An English description of the problem(s) should go here (Markdown is supported)"
+			></textarea>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label for="issueType" class="col-sm-2 control-label">Issue(s):</label>
+		<div class="col-sm-10">
+			<div class="input-group">
+				<select
+					id="issueType"
+					class="form-control"
+				>
+					<option>SQL injection</option>
+					<option>XSS</option>
+				</select>
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="button">+</button>
+					<button class="btn btn-default" type="button">-</button>
+				</span>
+			</div>
+			<!-- @todo How to do this in Bootstrap? -->
+			<div style="margin-top: 8px;">
+				<textarea
+					name="description"
+					id="issueDescription"
+					class="form-control"
+					rows="3"
+					placeholder="An optional English description of this issue can go here (Markdown is supported)"
+				></textarea>
+			</div>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label for="inputDate" class="col-sm-2 control-label">Author notified date:</label>
+		<div class="col-sm-10">
+			<input
+				type="date"
+				id="inputDate"
+				class="form-control"
+				name="author-notified-date"
+				placeholder="The date the author was notified (optional)"
+			/>
+		</div>
+	</div>
 
 	<input type="submit" value="Save" />
+
 </form>
