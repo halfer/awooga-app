171149f81e91091fa7e8c310a289cb9401c90aeb
Jon Hinks <projects.ws@jondh.me.uk>
2015-01-07 14:02:49 +0000

    Move validation methods to their own trait, to reduce complexity

--- src/Core/Report.php
+++ src/Core/Report.php
@@ -23,6 +23,7 @@ class Report
 
 	use \Awooga\Traits\Database;
 	use \Awooga\Traits\Runner;
+	use \Awooga\Traits\Validation;
 
 	/**
 	 * Creates this report and attaches it to a specific repo ID
@@ -624,55 +625,5 @@ class Report
 	protected function convertFromMarkdown($markdown)
 	{
 		return $markdown ? \Michelf\Markdown::defaultTransform($markdown) : null;
-	}
-
-	/**
-	 * Checks to see if the parameter is a string, raises a TrivialException if not
-	 * 
-	 * @param mixed $string
-	 */
-	protected function isString($string)
-	{
-		if (!is_string($string))
-		{
-			throw new TrivialException("This field is expected to be a string");			
-		}
-	}
-
-	/**
-	 * Checks to see if the parameter is an array, raises a TrivialException if not
-	 * 
-	 * @param mixed $array
-	 * @throws TrivialException
-	 */
-	protected function isArray($array)
-	{
-		if (!is_array($array))
-		{
-			throw new TrivialException("This field is expected to be an array");			
-		}		
-	}
-
-	/**
-	 * Checks to see if a required field is entered, raises a TrivialException if not
-	 * 
-	 * @param mixed $data
-	 * @param string $name
-	 * @throws TrivialException
-	 */
-	protected function isRequired($data, $name)
-	{
-		if (!$data)
-		{
-			throw new TrivialException("The '{$name}' field is required");
-		}		
-	}
-
-	protected function validateLength($data, $name, $length)
-	{
-		if (strlen($data) > $length)
-		{
-			throw new TrivialException("The '{$name}' field cannot be longer than {$length} characters");			
-		}
 	}
 }
