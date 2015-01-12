a7dfafab46dc7893c60d8b655e0f7ed538e8b8d9
Jon Hinks <projects.ws@jondh.me.uk>
2015-01-12 01:26:02 +0000

    Improve docblocks for Spiderling magic methods

--- test/browser/classes/TestCase.php
+++ test/browser/classes/TestCase.php
@@ -11,8 +11,9 @@ namespace Awooga\Testing\Browser;
  * @method string content() Return the content of the last request from the currently selected driver
  * @method string current_path() Return the current browser url without the domain
  * @method string current_url() Return the current url
- * @method \Openbuildings\Spiderling\Functest_Node assertHasCss($selector, array $filters = array(), $message = NULL)
+ * @method \Openbuildings\Spiderling\Node assertHasCss($selector, array $filters = array(), $message = NULL)
  * @method \Openbuildings\Spiderling\Node find($selector) Returns a single matching Node
+ * @method \Openbuildings\Spiderling\Node not_present($selector) Checks that a CSS expression is not found
  * @method array all($selector) Returns all matching elements as Nodes
  * @method void screenshot($filename) Takes a screenshot at this point in time
  */
