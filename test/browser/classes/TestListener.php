6ef8b414232c9d32d7fef0e9f8ba941115b5a2cb
Jon Hinks <projects.ws@jondh.me.uk>
2016-09-22 22:15:41 +0100

    Change to support multi-server Spiderling Utils methods

--- test/browser/classes/TestListener.php
+++ test/browser/classes/TestListener.php
@@ -2,10 +2,10 @@
 
 namespace Awooga\Testing\Browser;
 
+use halfer\SpiderlingUtils\Server;
+
 class TestListener extends \halfer\SpiderlingUtils\TestListener
 {
-	protected $hasInitialised = false;
-
 	/**
 	 * Specifies which suites and namespaces to respond to
 	 *
@@ -19,6 +19,16 @@ class TestListener extends \halfer\SpiderlingUtils\TestListener
 			(strpos($name, 'Awooga\\Testing\\Browser\\') !== false);
 	}
 
+	public function setupServers()
+	{
+		$projectRoot = $this->getProjectRoot();
+		$server = new Server($projectRoot . '/web');
+		$server->setRouterScriptPath($projectRoot . '/test/browser/scripts/router.php');
+		$server->setCheckAliveUri('/server-check');
+
+		$this->addServer($server);
+	}
+
 	public function runningBrowserTests()
 	{
 		parent::runningBrowserTests();
@@ -28,5 +38,10 @@ class TestListener extends \halfer\SpiderlingUtils\TestListener
 	protected function removeSearchIndex()
 	{
 		system('rm -rf ' . $this->getProjectRoot() . '/filesystem/tmp/search-index');
+	}
+
+	protected function getProjectRoot()
+	{
+		return realpath(__DIR__ . '/../../..');
 	}
 }
