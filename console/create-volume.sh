eb25a1108790c7198bbf1e68a232662426f566d9
Jon Hinks <projects.ws@jondh.me.uk>
2018-09-17 20:19:41 +0100

    Sketch out some volume creation code

--- console/create-volume.sh
+++ console/create-volume.sh
@@ -2,11 +2,16 @@
 #
 # Creates a volume on a Docker host
 
-# @todo Check if the volume exists already
-#if volume exists
-#    echo error
-#    exit
-#then
+VOLUME_NAME=awooga-volume
+
+# Check if the volume exists already
+docker volume inspect $VOLUME_NAME &> /dev/null
+VOLUME_RESULT=$?
+
+if [ $VOLUME_RESULT -eq 0 ]; then
+    echo Error: volume '$VOLUME_RESULT' already exists
+    exit 1
+fi
 
 # Create the volume
 docker volume create \
@@ -14,4 +19,4 @@ docker volume create \
     --opt o=size=50m \
     --opt type=tmpfs \
     --opt device=tmpfs \
-    awooga-volume
+    $VOLUME_NAME
