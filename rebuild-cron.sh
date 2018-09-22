ecce5024eba80a5ff8a88a28e112818de4c1b8ca
Jon Hinks <projects.ws@jondh.me.uk>
2018-09-22 15:59:07 +0100

    Finish rebuild scripts with search-and-replace device

--- rebuild-cron.sh
+++ rebuild-cron.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # The 'r' command inserts a whole file at the pattern
-sed -e '/__BASE__/r Dockerfile.base' Dockerfile.cron
+APP_NAME=cron
+sed \
+    -e '/__BASE__/r Dockerfile.base' \
+    Dockerfile.$APP_NAME \
+    > /tmp/awooga.Dockerfile.$APP_NAME
 
 docker build \
-    --file Dockerfile.cron \
-	--tag awooga \
+    --file /tmp/awooga.Dockerfile.$APP_NAME \
+	--tag awooga-$APP_NAME \
 	--target runtime \
 	.
--- rebuild-web.sh
+++ rebuild-web.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # The 'r' command inserts a whole file at the pattern
-sed -e '/__BASE__/r Dockerfile.base' Dockerfile.web
+APP_NAME=web
+sed \
+    -e '/__BASE__/r Dockerfile.base' \
+    Dockerfile.$APP_NAME \
+    > /tmp/awooga.Dockerfile.$APP_NAME
 
 docker build \
-    --file Dockerfile.web \
-	--tag awooga \
+    --file /tmp/awooga.Dockerfile.$APP_NAME \
+	--tag awooga-$APP_NAME \
 	--target runtime \
 	.
